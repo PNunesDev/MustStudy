b27ad521999a5e8e146ed5ff03a1bb43972dd71f
Pedro Nunes <nunes.p89@gmail.com>
2016-05-31 16:10:40 +0100

    Initial Deploy of the Website

---
+++ MustStudy/MustStudy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MustStudy.Startup))]
+namespace MustStudy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
